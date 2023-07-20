2699c660885999fd87bd70bbc9255c0804e54361
Finn Leo <finn.elias.leo@gmail.com>
2023-07-20 17:33:09 +0200

    Added Camera movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.Rendering.DebugUI;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float smoothTime;
+    [SerializeField] private Camera camera;
 
     private Rigidbody playerRb;
-    private float curVelocity;
-    private Vector2 moveVec;
-    private Vector3 dirVec;
+    public Vector2 moveVec;
+    public Vector3 dirVec;
+
+    [SerializeField] float mouseSens;
+    public Vector2 lookVec;
+    private float xValue;
+    private float yValue;
+
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -19,24 +26,33 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (moveVec.sqrMagnitude != 0)
-        {
+        
 
-            float yRotation = Mathf.Atan2(dirVec.x, dirVec.z) * Mathf.Rad2Deg;
-            float smoothYRot = Mathf.SmoothDampAngle(transform.eulerAngles.y, yRotation, ref curVelocity, smoothTime);
-            transform.rotation = Quaternion.Euler(0f, smoothYRot, 0f);
+            transform.rotation = Quaternion.Euler(0f, camera.transform.rotation.eulerAngles.y, 0f);
 
-            playerRb.velocity = dirVec * moveSpeed * Time.deltaTime;
-        }
-        else
-        {
-            playerRb.velocity = Vector3.zero;
-        }
+            playerRb.velocity = transform.TransformDirection(dirVec) * moveSpeed * Time.deltaTime;
+        
+    }
+
+    private void LateUpdate()
+    {
+        camera.transform.position = transform.position;
+        if (lookVec.sqrMagnitude == 0) return;
+        camera.transform.rotation = Quaternion.Euler(-yValue * mouseSens, xValue * mouseSens, 0f);
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         moveVec = context.ReadValue<Vector2>();
-        dirVec = new Vector3(moveVec.x, 0f, moveVec.y);
+        dirVec = new Vector3 (moveVec.x, 0f, moveVec.y);
+       
     }
+
+    public void MoveCamera(InputAction.CallbackContext context)
+    {
+        lookVec = context.ReadValue<Vector2>();
+        xValue += lookVec.x;
+        yValue += lookVec.y;
+    }
+
 }
